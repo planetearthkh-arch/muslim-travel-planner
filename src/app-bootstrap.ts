65fe64448d32813f32b4593e078d64f7920ff99c
planetearthkh-arch <planetearthkh@gmail.com>
2026-07-07 01:03:47 +0300

    Load extra bootstrap

--- src/app-bootstrap.ts
+++ src/app-bootstrap.ts
@@ -5,6 +5,7 @@ async function startApp() {
   await import('./main.js');
   await import('./halal-page-bootstrap.js');
   await import('./qibla-copy-bootstrap.js');
+  await import('./prayer-status-bootstrap.js');
 }
 
 void startApp();
